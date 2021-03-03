36310c394d6e78029744ad957f75573b2ae9938a
Tran Thong <thongt@dlu.edu.vn>
2021-03-03 13:00:19 +0700

    Test

---
+++ ChuongTrinhDauTien/BaiTap2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day la bai tap 2");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ChuongTrinhDauTien/ChuongTrinhDauTien/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChuongTrinhDauTien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dùng dấu // cho chú thích 1 dòng
+            Console.WriteLine("Chuong trinh dau tien");
+            /* Chú thích cho nhiều dòng */
+            Console.WriteLine("Dòng 1");
+            Console.WriteLine("Dòng 2");
+            Console.WriteLine("Dòng 3");
+            Console.ReadKey(); //Dừng màn hình chờ nhập 1 phím
+        }
+    }
+}
